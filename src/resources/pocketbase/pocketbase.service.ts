5982f16c2dda5654040860b2cd2f385ec755f892
Mikhail Ivannikov <mikhail.ivannikov@alphaott.com>
2024-12-10 18:39:37 +0300

    chore: Change pocketbase url

--- src/resources/pocketbase/pocketbase.service.ts
+++ src/resources/pocketbase/pocketbase.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const PocketBase = require('pocketbase/cjs');
 
-const pb = new PocketBase('https://api.spendify.ivannikoff.ru/');
+const pb = new PocketBase('https://pocketbase.spendify.ru/');
 
 @Injectable()
 export class PocketBaseService {
